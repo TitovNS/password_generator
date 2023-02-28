eca33ce2e466bce66403a25b9a503c38dcc0e7d9
TitovNS <n.titov@3h.ru>
2023-02-28 22:53:35 +0600

    add nginx

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -1,7 +1,13 @@
+from django.conf.urls.static import static
 from django.urls import path
 from generator import views
+
+from password_generator import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('password/', views.password, name='password'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
